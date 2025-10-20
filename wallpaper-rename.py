9b92084b23e8d7ddd5baba1bb7101e9518a65a06
Aaron <117947303+aaronbehan@users.noreply.github.com>
2025-10-20 23:26:31 +0100

    initial commit
    Adds a script to rename wallpaper files to avoid name clashes.

---
+++ wallpaper-rename.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+
+path = "/path/to/wallpapers"
+
+directory = os.listdir(path)
+
+# renaming them so that i do not get clashes with names. for example, i want to name files 0, 1, 2, 3 but those names are occupied
+for file in directory:
+    os.rename(f"{path}{file}", f"{path}t{file}")  # t for temporary
+
+
+for x in range(len(directory)):
+    os.rename(f"{path}t{directory[x]}", f"{path}{len(directory) - x}.jpg")
